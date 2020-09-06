7355479225ee49857e0604797e7ba985084c9c17
jphari <info@datahari.fi>
2020-09-06 06:23:36 +0300

    change place order button to it's original state if Laskuhari payment method is not selected payment method change detection has been moved inline

--- js/public.js
+++ js/public.js
@@ -11,12 +11,17 @@ function laskuhari_tarkista_verkkolaskuosoite($) {
 	}
 }
 
+var lh_place_order_original_value = null;
 function laskuhari_tarkista_laskutustapa($){
+    if( lh_place_order_original_value === null ) {
+        lh_place_order_original_value = $("#place_order").prop("disabled");
+    }
+    $("#place_order").prop("disabled", lh_place_order_original_value);
     if( $("#payment_method_laskuhari").is(":checked") ) {
         if( $("#laskuhari-laskutustapa").val() == "" || ($("#laskuhari-laskutustapa").val() == "verkkolasku" && $("#laskuhari-ytunnus").val() == "")) {
             $("#place_order").prop("disabled", true);
         } else {
-            $("#place_order").prop("disabled", false);
+            $("#place_order").prop("disabled", lh_place_order_original_value);
         }
     }
 }
@@ -25,12 +30,6 @@ function laskuhari_tarkista_laskutustapa($){
 	$(document).ready(function() {
         $(".verkkolasku-pakollinen").bind("keyup change", function(){
             laskuhari_tarkista_laskutustapa($);
-        });
-        $("#payment_method_laskuhari, #payment").on("change click", function() {
-            if( $("#payment_method_laskuhari").prop("checked") != laskuhari_viime_maksutapa ) {
-                $('body').trigger('update_checkout');
-                laskuhari_viime_maksutapa = $("#payment_method_laskuhari").prop("checked");
-            }
         });
     });
 })(jQuery);
