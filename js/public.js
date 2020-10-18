cb310455aece4c3cd76b61fed1b3a417311514ed
datahari <info@datahari.fi>
2020-10-18 06:32:12 +0300

    made improvements to javascript

--- js/public.js
+++ js/public.js
@@ -1,35 +1,40 @@
 var laskuhari_viime_maksutapa = false;
 
-function laskuhari_tarkista_verkkolaskuosoite($) {
-	if( typeof laskuhari_tarkista_laskutustapa === "function" ) laskuhari_tarkista_laskutustapa($);
-	if( $("#laskuhari-laskutustapa").val() == "verkkolasku" ) {
-		$("#laskuhari-verkkolasku-tiedot").show();
-		$(".verkkolasku-pakollinen").attr("required", true);
-	} else {
-		$("#laskuhari-verkkolasku-tiedot").hide();
-		$(".verkkolasku-pakollinen").attr("required", false);
-	}
-}
-
-var lh_place_order_original_value = null;
-function laskuhari_tarkista_laskutustapa($){
-    if( lh_place_order_original_value === null ) {
-        lh_place_order_original_value = $("#place_order").prop("disabled");
-    }
-    $("#place_order").prop("disabled", lh_place_order_original_value);
-    if( $("#payment_method_laskuhari").is(":checked") ) {
-        if( $("#laskuhari-laskutustapa").val() == "" || ($("#laskuhari-laskutustapa").val() == "verkkolasku" && $("#laskuhari-ytunnus").val() == "")) {
-            $("#place_order").prop("disabled", true);
+(function($) {
+    function laskuhari_tarkista_verkkolaskuosoite() {
+        if( $("#laskuhari-laskutustapa").val() == "verkkolasku" ) {
+            $("#laskuhari-verkkolasku-tiedot").show();
+            $(".verkkolasku-pakollinen").attr("required", true);
         } else {
-            $("#place_order").prop("disabled", lh_place_order_original_value);
+            $("#laskuhari-verkkolasku-tiedot").hide();
+            $(".verkkolasku-pakollinen").attr("required", false);
         }
     }
-}
-
-(function($) {
+    function laskuhari_tarkista_laskutustapa(){
+        if( $("#payment_method_laskuhari").is(":checked") ) {
+            if( $("#laskuhari-laskutustapa").val() == "" || ($("#laskuhari-laskutustapa").val() == "verkkolasku" && $("#laskuhari-ytunnus").val() == "")) {
+                $("#place_order:enabled").prop("disabled", true).addClass("laskuhari-place-order-disabled");
+            } else {
+                $("#place_order.laskuhari-place-order-disabled").prop("disabled", false).removeClass("laskuhari-place-order-disabled");
+            }
+        }
+    }
 	$(document).ready(function() {
-        $(".verkkolasku-pakollinen").bind("keyup change", function(){
-            laskuhari_tarkista_laskutustapa($);
+        $("body").on("keyup change", ".verkkolasku-pakollinen", function(){
+            laskuhari_tarkista_laskutustapa();
+        });
+        $("body").bind("updated_checkout", function(){
+            laskuhari_tarkista_laskutustapa();
+        });
+        $("body").on("change click", "#payment_method_laskuhari, #payment", function() {
+            laskuhari_tarkista_laskutustapa();
+            if( $("#payment_method_laskuhari").prop("checked") != laskuhari_viime_maksutapa ) {
+                $('body').trigger('update_checkout');
+                laskuhari_viime_maksutapa = $("#payment_method_laskuhari").prop("checked");
+            }
+        });
+        $("body").on("keyup change", "#laskuhari-laskutustapa", function() {
+            laskuhari_tarkista_verkkolaskuosoite();
         });
     });
 })(jQuery);
