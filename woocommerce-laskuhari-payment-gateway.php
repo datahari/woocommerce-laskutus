44478c42efa95ef254d72cf27bddfd272031180c
Datahari <info@datahari.fi>
2022-08-02 09:51:31 +0300

    changed from triple equals to double equals because value stored in database may be string or int

--- woocommerce-laskuhari-payment-gateway.php
+++ woocommerce-laskuhari-payment-gateway.php
@@ -129,7 +129,7 @@ function lh_create_select_box( $name, $options, $current = '' ) {
     $html = '<select name="'.esc_attr( $name ).'">';
     foreach( $options as $value => $text ) {
         $html .= '<option value="'.esc_attr( $value ).'"';
-        if( $current === $value ) {
+        if( $current == $value ) {
             $html .= " selected";
         }
         $html .= '>'.esc_html( $text ).'</option>';
