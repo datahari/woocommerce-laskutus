90092d85a9cf13afd28fec473ab3f00c401b2366
Datahari <info@datahari.fi>
2022-08-19 15:39:46 +0300

    added function and variable prefixes for compatibility with other plugins

--- updater.php
+++ updater.php
@@ -7,22 +7,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 Plugin updater by Jeremy Clark: https://github.com/jeremyclark13/automatic-theme-plugin-update
 */
 
-$api_url = 'https://www.laskuhari.fi/wc/api/';
-$plugin_slug = plugin_basename( dirname( __FILE__ ) );
+$laskuhari_wc_api_url = 'https://www.laskuhari.fi/wc/api/';
+$laskuhari_plugin_slug = plugin_basename( dirname( __FILE__ ) );
 
 // Take over the update check
-add_filter( 'pre_set_site_transient_update_plugins', 'check_for_plugin_update' );
+add_filter( 'pre_set_site_transient_update_plugins', 'laskuhari_check_for_plugin_update' );
 
-function check_for_plugin_update( $checked_data ) {
-	global $api_url, $plugin_slug, $wp_version;
+function laskuhari_check_for_plugin_update( $checked_data ) {
+	global $laskuhari_wc_api_url, $laskuhari_plugin_slug, $wp_version;
 
-	if ( empty( $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'] ) ) {
+	if ( empty( $checked_data->checked[$laskuhari_plugin_slug .'/'. $laskuhari_plugin_slug .'.php'] ) ) {
 		return $checked_data;
 	}
 
 	$args = array(
-		'slug' => $plugin_slug,
-		'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'],
+		'slug' => $laskuhari_plugin_slug,
+		'version' => $checked_data->checked[$laskuhari_plugin_slug .'/'. $laskuhari_plugin_slug .'.php'],
 	);
 
 	$request_string = array(
@@ -35,7 +35,7 @@ function check_for_plugin_update( $checked_data ) {
 	);
 
 	// Start checking for an update
-	$raw_response = wp_remote_post( $api_url, $request_string );
+	$raw_response = wp_remote_post( $laskuhari_wc_api_url, $request_string );
 
 	if ( ! is_wp_error( $raw_response ) && ( $raw_response['response']['code'] == 200 ) ) {
 		$response = unserialize( $raw_response['body'] );
@@ -43,7 +43,7 @@ function check_for_plugin_update( $checked_data ) {
 
 	// Feed the update data into WP updater
 	if ( is_object( $response ) && ! empty( $response ) ) {
-		$checked_data->response[$plugin_slug .'/'. $plugin_slug .'.php'] = $response;
+		$checked_data->response[$laskuhari_plugin_slug .'/'. $laskuhari_plugin_slug .'.php'] = $response;
 	}
 
 	return $checked_data;
@@ -51,18 +51,18 @@ function check_for_plugin_update( $checked_data ) {
 
 
 // Take over the Plugin info screen
-add_filter( 'plugins_api', 'plugin_api_call', 10, 3 );
+add_filter( 'plugins_api', 'laskuhari_plugin_api_call', 10, 3 );
 
-function plugin_api_call( $def, $action, $args ) {
-	global $plugin_slug, $api_url, $wp_version;
+function laskuhari_plugin_api_call( $def, $action, $args ) {
+	global $laskuhari_plugin_slug, $laskuhari_wc_api_url, $wp_version;
 
-	if ( ! isset( $args->slug ) || ( $args->slug != $plugin_slug ) ) {
+	if ( ! isset( $args->slug ) || ( $args->slug != $laskuhari_plugin_slug ) ) {
 		return false;
 	}
 
 	// Get the current version
 	$plugin_info = get_site_transient( 'update_plugins' );
-	$current_version = $plugin_info->checked[$plugin_slug .'/'. $plugin_slug .'.php'];
+	$current_version = $plugin_info->checked[$laskuhari_plugin_slug .'/'. $laskuhari_plugin_slug .'.php'];
 	$args->version = $current_version;
 
 	$request_string = array(
@@ -74,7 +74,7 @@ function plugin_api_call( $def, $action, $args ) {
 		'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' )
 	);
 
-	$request = wp_remote_post( $api_url, $request_string );
+	$request = wp_remote_post( $laskuhari_wc_api_url, $request_string );
 
 	if ( is_wp_error( $request ) ) {
 		$res = new WP_Error( 'plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message() );
