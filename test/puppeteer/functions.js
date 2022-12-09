810672ec8b95b616e46f644e205ed75b93c1899c
Datahari <info@datahari.fi>
2022-12-09 07:33:06 +0200

    e2e tests: moved manual order creation to its own function

--- test/puppeteer/functions.js
+++ test/puppeteer/functions.js
@@ -1,5 +1,9 @@
 const config = require( "./config.js" );
 
+const accept_dialog = dialog => {
+    dialog.accept("1.43");
+};
+
 exports.login = async function( page ) {
     await page.waitFor( "#user_login" );
     await page.click( "#user_login" );
@@ -158,4 +162,87 @@ exports.reset_settings = async function( page ) {
         $("#woocommerce_laskuhari_status_after_gateway").val("processing");
         $("#woocommerce_laskuhari_status_after_paid").val("");
     } );
+}
+
+exports.create_manual_order = async function( page ) {
+    // go to new order creation
+    await page.goto( config.wordpress_url + "/wp-admin/post-new.php?post_type=shop_order" );
+
+    // click billing address edit button
+    await page.waitFor( ".order_data_column a.edit_address" );
+    await page.click( ".order_data_column a.edit_address" );
+
+    // input customer details
+    await page.waitFor( "#_billing_first_name" );
+    await page.click( "#_billing_first_name" );
+    await page.keyboard.type( "Jack" );
+    await page.click( "#_billing_last_name" );
+    await page.keyboard.type( "Smith" );
+    await page.click( "#_billing_address_1" );
+    await page.keyboard.type( "Jack's road" );
+    await page.click( "#_billing_city" );
+    await page.keyboard.type( "Jackcity" );
+    await page.click( "#_billing_postcode" );
+    await page.keyboard.type( "54321" );
+    await page.click( "#_billing_email" );
+    await page.keyboard.type( config.test_email );
+
+    // click "Add line item"
+    await page.click( ".button.add-line-item" );
+    await page.waitFor( 600 );
+
+    // click "Add product"
+    await page.click( ".button.add-order-item" );
+    await page.waitFor( 400 );
+
+    // click "Search for a product"
+    await page.hover( ".wc-backbone-modal-content .select2-selection--single" );
+    await page.waitFor( 200 );
+    await page.click( ".wc-backbone-modal-content .select2-selection--single" );
+    await page.waitFor( 700 );
+
+    // input search keyword
+    await page.click( ".select2-container .select2-search__field[aria-expanded=true]" );
+    await page.keyboard.type( "Hoodie" );
+
+    // wait for results
+    await page.waitFor( ".select2-results__option[role=option]:not(.loading-results)" );
+
+    // click on first result
+    await page.click( ".select2-results__option[role=option]:not(.loading-results)" );
+
+    // click on add product
+    await page.click( ".wc-backbone-modal-content .button.button-primary.button-large" );
+
+    // wait for product to appear in list
+    await page.waitFor( "#order_line_items tr.item" );
+    await page.waitFor( 1000 );
+
+    // click "Add line item"
+    await page.click( ".button.add-line-item" );
+    await page.waitFor( 600 );
+
+    // prepare for the fee amount prompt
+    page.off('dialog', accept_dialog);
+    page.on('dialog', accept_dialog);
+
+    // click "Add fee"
+    await page.click( ".button.add-order-fee" );
+    await page.waitFor( 2000 );
+
+    await page.waitForSelector( ".button.button-primary.calculate-action", {
+        visible: true
+    } );
+
+    // click on "calculate"
+    await page.click( ".button.button-primary.calculate-action" );
+
+    // wait for changes to take effect
+    await page.waitFor( 3000 );
+
+    // create order
+    await page.click( ".button.save_order.button-primary" );
+
+    // wait for page to load
+    await page.waitFor( "#laskuhari_metabox" );
 }
