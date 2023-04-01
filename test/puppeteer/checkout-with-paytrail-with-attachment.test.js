6fbdaba034844f72958cafd7f6692b80fca29573
datahari <info@datahari.fi>
2023-04-01 09:47:52 +0300

    added e2e tests for creating invoice from other payment methods

---
+++ test/puppeteer/checkout-with-paytrail-with-attachment.test.js
@@ -0,0 +1,87 @@
+const puppeteer = require('puppeteer');
+const functions = require('./functions.js');
+const config = require('./config.js');
+
+/**
+ * Test sending an invoice as attachment along with an order that is paid
+ * with the Paytrail payment method.
+ */
+
+test("checkout-with-paytrail", async () => {
+    const browser = await puppeteer.launch({
+        headless: config.headless,
+        defaultViewport: {
+            width: 1452,
+            height: 768
+        },
+        args:[
+            '--start-maximized'
+        ]
+    });
+
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout( 60000 );
+
+    page.on("pageerror", function(err) {
+            theTempValue = err.toString();
+            console.log("Page error: " + theTempValue);
+            browser.close();
+    });
+
+    // log in to plugin settings page
+    await functions.open_settings( page );
+
+    // reset settings
+    await functions.reset_settings( page );
+
+    // select settings
+    await page.evaluate( function() {
+        let $ = jQuery;
+        $("#woocommerce_laskuhari_send_invoice_from_payment_methods option[value='paytrail']").attr( "selected", true );
+        $("#woocommerce_laskuhari_attach_receipt_to_wc_email").prop( "checked", true );
+        $("#woocommerce_laskuhari_auto_gateway_create_enabled").prop( "checked", true );
+        $("#woocommerce_laskuhari_auto_gateway_enabled").prop( "checked", false );
+        $("#woocommerce_laskuhari_laskuviesti").val(
+            $("#woocommerce_laskuhari_laskuviesti").val() +
+            " (paytrail-with-attachment-test)"
+        );
+        $("#woocommerce_laskuhari_instructions").val(
+            $("#woocommerce_laskuhari_instructions").val() +
+            " (paytrail-with-attachment-test)"
+        );
+    } );
+
+    // save settings
+    await page.click( ".woocommerce-save-button" );
+
+    // wait for settings to be saved
+    await page.waitForNavigation();
+    await page.waitForSelector( ".updated.inline" );
+
+    // log out
+    await functions.logout( page );
+
+    // make an order
+    await functions.add_product_to_cart_and_go_to_checkout( page );
+    await functions.fill_out_checkout_form( page );
+    await functions.select_paytrail_payment_method( page );
+    await functions.place_order( page );
+
+    // wait for order to be placed
+    await page.waitForSelector( ".woocommerce-order-received" );
+
+    // grab order id
+    let order_id = await functions.grab_order_id( page );
+
+    // go to orders list
+    await page.goto( config.wordpress_url + "/wp-admin/edit.php?post_type=shop_order" );
+    await functions.login( page, config.wordpress_user, config.wordpress_password );
+
+    // check that the order was added to the list
+    await functions.wait_for_loading( page );
+    let element = await page.$('#post-'+order_id);
+    expect(element).toBeTruthy();
+
+    // close browser
+    await browser.close();
+}, 600000);
