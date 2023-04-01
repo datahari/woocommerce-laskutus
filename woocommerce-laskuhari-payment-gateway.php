537495f0763d2f855122c4c6af5c9eada25dfa06
datahari <info@datahari.fi>
2023-04-01 05:33:17 +0300

    bumped version number

--- woocommerce-laskuhari-payment-gateway.php
+++ woocommerce-laskuhari-payment-gateway.php
@@ -3,7 +3,7 @@
 Plugin Name: Laskuhari for WooCommerce
 Plugin URI: https://www.laskuhari.fi/woocommerce-laskutus
 Description: Lisää automaattilaskutuksen maksutavaksi WooCommerce-verkkokauppaan sekä mahdollistaa tilausten manuaalisen laskuttamisen
-Version: 1.8.1
+Version: 1.9.0
 Author: Datahari Solutions
 Author URI: https://www.datahari.fi
 License: GPLv2
