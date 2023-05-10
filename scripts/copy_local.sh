a51b7abad5aef0bdd2a729f792f0e9729139744c
datahari <info@datahari.fi>
2023-05-10 11:15:24 +0300

    added local dev scripts

---
+++ scripts/copy_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# copies local files to test environment wordpress installation
+
+cd "$(dirname "$BASH_SOURCE")"
+
+sudo rsync -av --delete ../ ../test/assets/wordpress/wp-content/plugins/woocommerce-laskuhari-payment-gateway/ \
+     --exclude="test/" \
+     --exclude="vendor/" \
+     --exclude=".git/" \
+     --exclude="scripts/" \
+     --exclude="config/" \
+     --exclude="*.zip"
+sudo chown -R --reference=../test/assets/wordpress/wp-content/index.php ../test/assets/wordpress/wp-content/plugins/woocommerce-laskuhari-payment-gateway/
---
+++ scripts/create_local_package.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#############################################################################
+#                                                                           #
+#      THIS SCRIPT GENERATES THE PLUGIN ZIP FILE FROM THE LOCAL SOURCE      #
+#                                                                           #
+#############################################################################
+
+cd "$(dirname "$BASH_SOURCE")"
+
+# if folder with git repository name already exists, quit
+# be cause we don't want to delete or overwrite anything!
+if [ -e "woocommerce-laskuhari-payment-gateway" ]; then
+    echo "Folder woocommerce-laskuhari-payment-gateway already exists!"
+    exit 0
+fi
+
+# copy local code
+rsync -av ../ woocommerce-laskuhari-payment-gateway/ \
+      --exclude="test/" \
+      --exclude="vendor/" \
+      --exclude=".git/" \
+      --exclude="scripts/" \
+      --exclude="config/" \
+      --exclude="*.zip"
+
+# remove git folder and files
+rm -rf woocommerce-laskuhari-payment-gateway/.git
+rm -rf woocommerce-laskuhari-payment-gateway/.gitignore
+
+# remove README.md
+rm -rf woocommerce-laskuhari-payment-gateway/README.md
+
+# remove tests
+rm -rf woocommerce-laskuhari-payment-gateway/test
+
+# remove scripts
+rm -rf woocommerce-laskuhari-payment-gateway/scripts
+
+# get version number
+VERSION=`grep "Version: " woocommerce-laskuhari-payment-gateway/woocommerce-laskuhari-payment-gateway.php | awk '{print $2}'`
+
+# create zip arhive named with version number
+zip -r woocommerce-laskuhari-payment-gateway.$VERSION.zip woocommerce-laskuhari-payment-gateway
+
+# remove original folder
+rm -rf woocommerce-laskuhari-payment-gateway
